a9ffaa17b8fce775ea63d5b191c6017630de2fef
ruchi-kr <ruchi.kumari63@gmail.com>
2024-04-23 09:42:47 +0530

    complete till employee dts

--- Routes/employee_route.js
+++ Routes/employee_route.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require("mysql");
-const connection =require("../db");
+const connection = require("../db");
 
 // API FOR CRUD EMPLOYEE
 
 // CREATE
 router.post('/api/user/addTask', (req, res) => {
-    const { project_name, task,start_time, end_time,status,remarks } = req.body;
-    const query = 'INSERT INTO employee ( project_name, task,start_time, end_time,status,remarks ) VALUES (?, ?, ?,?,?,?)';
-    connection.query(query, [project_name, task,start_time, end_time,status,remarks], (err, results) => {
+    const { project_name, task, allocated_time, actual_time, end_time, status, remarks } = req.body;
+    const query = 'INSERT INTO employee ( project_name, task,allocated_time, actual_time,status,remarks ) VALUES (?, ?, ?,?,?,?)';
+    connection.query(query, [project_name, task, allocated_time, actual_time, status, remarks], (err, results) => {
         if (err) throw err;
         res.status(200).send('Task Added Successfully');
     });
@@ -25,7 +25,27 @@ router.get('/api/user/getTasks', (req, res) => {
 });
 
 // EDIT
+// UPDATE
+router.put('/api/user/updateTask/:taskId', (req, res) => {
+    const taskId = req.params.taskId;
+    const { project_name, task, allocated_time, actual_time, status, remarks } = req.body;
+    const query = 'UPDATE employee SET project_name = ?, task = ?, allocated_time = ?, actual_time = ?, status = ?, remarks = ? WHERE id = ?';
+    connection.query(query, [project_name, task, allocated_time, actual_time, status, remarks, taskId], (err, results) => {
+        if (err) throw err;
+        res.status(200).send('Task Updated Successfully');
+    });
+});
+
 // DELETE
+router.delete('/api/user/deleteTask/:taskId', (req, res) => {
+    const taskId = req.params.taskId;
+    const query = 'DELETE FROM employee WHERE id = ?';
+    connection.query(query, [taskId], (err, results) => {
+        if (err) throw err;
+        res.status(200).send('Task Deleted Successfully');
+    });
+});
+
 
 
 
