d2e540db8595cb145a3146e1d1cc4cd6f674e655
Tarun Sachan <tarunsachan@hotmail.com>
2024-04-23 12:21:35 +0530

    add teams

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const mysql = require("mysql");
 const app = express();
 const port = 8000;
 
-
 app.use(cors());
 app.use(express.json());
 
-app.use(morgan("tiny"));
-
+// app.use(morgan("tiny"));
+const teamsRoutes = require("./Routes/teamsRoutes");
 // MySQL connection
 // const connection = mysql.createConnection({
 //     host: 'localhost',
@@ -19,21 +18,21 @@ app.use(morgan("tiny"));
 //     password: '',
 //     database: 'performancems'
 //   });
-  
+
 //   connection.connect(err => {
 //     if (err) throw err;
 //     console.log('Connected to MySQL database');
 //   });
 
-
-  app.use(require('./Routes/Auth_route'));
-  app.use(require('./Routes/Employeemaster_route'));
-  app.use(require('./Routes/usermaster_route'));
-  app.use(require('./Routes/rm_master_route'));
-  app.use(require('./Routes/projectmaster_route'));
-
+app.use(require("./Routes/Auth_route"));
+app.use(require("./Routes/Employeemaster_route"));
+app.use(require("./Routes/usermaster_route"));
+app.use(require("./Routes/rm_master_route"));
+app.use(require("./Routes/projectmaster_route"));
+//Teams routes
+app.use("/api/user", teamsRoutes);
   app.use(require('./Routes/employee_route'));
 
-app.listen( port, ()=>{
-    console.log(`Listening on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
