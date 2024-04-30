45abe3a1a6a254695eda9a4f1dc1c744caf5b96c
ruchi-kr <ruchi.kumari63@gmail.com>
2024-04-30 09:43:28 +0530

    updated code

--- Routes/employee_route.js
+++ Routes/employee_route.js
@@ -102,10 +102,61 @@ router.delete("/api/user/deleteTask/:taskId", (req, res) => {
   });
 });
 
+// router.get("/api/user/getReports/:employee_id", (req, res) => {
+//   const employee_id = req.params.employee_id;
+
+//   const { page, pageSize, name = "" } = req.query;
+
+//   // Validate page and pageSize
+//   if (!page || isNaN(page) || !pageSize || isNaN(pageSize)) {
+//     return res.status(400).send("Invalid page or pageSize");
+//   }
+
+//   const offset = (parseInt(page) - 1) * parseInt(pageSize);
+
+
+//   const query = `SELECT e.project_id, 
+// SUM(e.allocated_time) AS total_allocated_hours, 
+// SUM(e.actual_time) AS total_actual_hours,
+// p.schedule_start_date,
+// p.schedule_end_date,
+// p.project_name,
+// (
+//   SELECT CONCAT(
+//       '[',
+//       GROUP_CONCAT(
+//           JSON_OBJECT(
+//               'task', t.task, 
+//               'created_at', t.created_at,
+//               'status', t.status, 
+//               'allocated_time', t.allocated_time, 
+//               'actual_time', t.actual_time
+//           )
+//       ),
+//       ']'
+//   )
+//   FROM employee t
+//   WHERE t.project_id = e.project_id
+// )AS tasks
+// FROM employee e
+// JOIN project_master p ON e.project_id = p.project_id
+// WHERE e.user_id = ?
+// AND p.project_name LIKE '%${name}%'
+// GROUP BY e.project_id
+// LIMIT ? OFFSET ?`;
+
+
+//   connection.query(query, [employee_id, parseInt(pageSize), offset], (err, results) => {
+//     if (err) throw err;
+
+//     res.status(200).json(results);
+//   });
+// });
+
+
 router.get("/api/user/getReports/:employee_id", (req, res) => {
   const employee_id = req.params.employee_id;
-
-  const { page, pageSize, name = "" } = req.query;
+  const { page, pageSize, name = "", fromDate="", toDate="" } = req.query;
 
   // Validate page and pageSize
   if (!page || isNaN(page) || !pageSize || isNaN(pageSize)) {
@@ -114,45 +165,49 @@ router.get("/api/user/getReports/:employee_id", (req, res) => {
 
   const offset = (parseInt(page) - 1) * parseInt(pageSize);
 
-
-  const query = `SELECT e.project_id, 
-SUM(e.allocated_time) AS total_allocated_hours, 
-SUM(e.actual_time) AS total_actual_hours,
-p.schedule_start_date,
-p.schedule_end_date,
-p.project_name,
-(
-  SELECT CONCAT(
-      '[',
-      GROUP_CONCAT(
-          JSON_OBJECT(
-              'task', t.task, 
+  const query = `
+    SELECT e.project_id,
+      SUM(e.allocated_time) AS total_allocated_hours,
+      SUM(e.actual_time) AS total_actual_hours,
+      p.schedule_start_date,
+      p.schedule_end_date,
+      p.project_name,
+      (
+        SELECT CONCAT(
+          '[',
+          GROUP_CONCAT(
+            JSON_OBJECT(
+              'task', t.task,
               'created_at', t.created_at,
-              'status', t.status, 
-              'allocated_time', t.allocated_time, 
+              'status', t.status,
+              'allocated_time', t.allocated_time,
               'actual_time', t.actual_time
-          )
-      ),
-      ']'
-  )
-  FROM employee t
-  WHERE t.project_id = e.project_id
-)AS tasks
-FROM employee e
-JOIN project_master p ON e.project_id = p.project_id
-WHERE e.user_id = ?
-AND p.project_name LIKE '%${name}%'
-GROUP BY e.project_id
-LIMIT ? OFFSET ?`;
+            )
+          ),
+          ']'
+        )
+        FROM employee t
+        WHERE t.project_id = e.project_id 
+       
+        ${fromDate ? `AND (t.created_at >= ? OR t.created_at IS NULL)` : ''} 
+        ${toDate ? `AND (t.created_at <= ? OR t.created_at IS NULL)` : ''}
+      ) AS tasks
+      FROM employee e
+      JOIN project_master p ON e.project_id = p.project_id
+      WHERE e.user_id = ?
+      AND p.project_name LIKE '%${name}%'
+      GROUP BY e.project_id
+      LIMIT ? OFFSET ? `;
 
-
-  connection.query(query, [employee_id, parseInt(pageSize), offset], (err, results) => {
-    if (err) throw err;
-
-    res.status(200).json(results);
-  });
+  connection.query(
+    query,
+    [fromDate, toDate, employee_id, parseInt(pageSize), offset],
+    (err, results) => {
+      if (err) throw err;
+      res.status(200).json(results);
+    }
+  );
 });
-
 
 
 module.exports = router;
