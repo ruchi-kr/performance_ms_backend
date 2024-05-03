2f5775ac00a2b160f69981aaba186f53c0c7dbc4
Tarun Sachan <tarunsachan@hotmail.com>
2024-05-03 11:22:19 +0530

    fix manager emp wise report

--- controllers/employeeReportToManager.js
+++ controllers/employeeReportToManager.js
@@ -3,7 +3,96 @@ const connection = require("../db");
 
 const ViewProjectReport = (req, res) => {
   const { reporting_manager_id } = req.params;
-  console.log("{reporting_manager_id}", reporting_manager_id);
+  const { toDate, fromDate, search } = req.query;
+  console.log("Search terms", search);
+  console.log("toDate", toDate, "---fromDate:", fromDate);
+  let altQuery = "";
+  if (
+    (toDate === null || toDate === "null" || toDate === undefined) &&
+    (fromDate === null || fromDate === "null" || fromDate === undefined)
+  ) {
+    console.log("running default date range");
+    altQuery = `
+    SELECT 
+    e.employee_id, 
+    em.name,
+    em.manager_id,
+    SUM(e.allocated_time) AS total_allocated_time,
+    SUM(e.actual_time) AS total_actual_time,
+    CONCAT(
+        '[',
+        GROUP_CONCAT(
+            CONCAT(
+                '{', 
+                '"task_id":', e.id, 
+                ', "task":"', e.task, 
+                '", "allocated_time":', e.allocated_time,  
+                ', "actual_time":', e.actual_time,                 
+                ', "status":"', e.status, 
+                '", "project_id":', e.project_id, 
+                ', "project_name":"', pm.project_name, 
+                '", "created_at":"', DATE_FORMAT(e.created_at, '%Y-%m-%d %H:%i:%s'), 
+                '"}'
+            ) SEPARATOR ', '
+        ),
+        ']'
+    ) AS tasks_details
+FROM 
+    employee AS e
+LEFT JOIN 
+    project_master AS pm ON e.project_id = pm.project_id
+LEFT JOIN
+    employee_master AS em ON em.employee_id = e.employee_id
+WHERE 
+    e.manager_id = ?
+    AND e.created_at >= DATE_ADD(NOW(), INTERVAL -90 DAY)
+    AND
+    (LOWER(em.name) LIKE LOWER(CONCAT('%', ?, '%')) OR ? = '')
+GROUP BY 
+    e.employee_id;
+    `;
+  } else {
+    console.log("Running specific date range query");
+    altQuery = `
+    SELECT 
+    e.employee_id, 
+    em.name,
+    em.manager_id,
+    SUM(e.allocated_time) AS total_allocated_time,
+    SUM(e.actual_time) AS total_actual_time,
+    CONCAT(
+        '[',
+        GROUP_CONCAT(
+            CONCAT(
+                '{', 
+                '"task_id":', e.id, 
+                ', "task":"', e.task, 
+                '", "allocated_time":', e.allocated_time,  
+                ', "actual_time":', e.actual_time,                 
+                ', "status":"', e.status, 
+                '", "project_id":', e.project_id, 
+                ', "project_name":"', pm.project_name, 
+                '", "created_at":"', DATE_FORMAT(e.created_at, '%Y-%m-%d %H:%i:%s'), 
+                '"}'
+            ) SEPARATOR ', '
+        ),
+        ']'
+    ) AS tasks_details
+FROM 
+    employee AS e
+LEFT JOIN 
+    project_master AS pm ON e.project_id = pm.project_id
+LEFT JOIN
+    employee_master AS em ON em.employee_id = e.employee_id
+WHERE 
+    e.manager_id = ?
+    AND LOWER(em.name) LIKE LOWER(CONCAT('%', ?, '%'))
+    AND DATE(e.created_at) BETWEEN ? AND ?
+   
+GROUP BY 
+    e.employee_id;
+    `;
+  }
   const query = `
   SELECT 
     e.employee_id, 
@@ -19,11 +108,11 @@ const ViewProjectReport = (req, res) => {
                 '"task_id":', e.id, 
                 ', "task":"', e.task, 
                 '", "allocated_time":', e.allocated_time,  
-                ', "actual_time":', e.actual_time, 
-                
+                ', "actual_time":', e.actual_time,                 
                 ', "status":"', e.status, 
                 '", "project_id":', e.project_id, 
                 ', "project_name":"', pm.project_name, 
+                '", "created_at":"', DATE_FORMAT(e.created_at, '%Y-%m-%d %H:%i:%s'), 
                 '"}'
             ) SEPARATOR ', '
         ),
@@ -37,22 +126,29 @@ LEFT JOIN
     employee_master AS em ON em.employee_id = e.employee_id
 WHERE 
     e.manager_id = ?
+    AND LOWER(em.name) LIKE LOWER(CONCAT('%', ?, '%'))
+    AND e.created_at >= DATE_ADD(NOW(), INTERVAL -90 DAY)
+    
 GROUP BY 
     e.employee_id;
   `;
 
   try {
-    connection.query(query, reporting_manager_id, (err, results) => {
-      results = JSON.parse(JSON.stringify(results));
-      const temp = results.map((item) => {
-        console.log("obj", item);
-        return {
-          ...item,
-          tasks_details: JSON.parse(item.tasks_details),
-        };
-      });
-      res.status(StatusCodes.OK).json(results);
-    });
+    connection.query(
+      altQuery,
+      [reporting_manager_id, search, toDate, fromDate],
+      (err, results) => {
+        results = JSON.parse(JSON.stringify(results));
+        const temp = results.map((item) => {
+          // console.log("obj", item);
+          return {
+            ...item,
+            tasks_details: JSON.parse(item.tasks_details),
+          };
+        });
+        res.status(StatusCodes.OK).json(temp);
+      }
+    );
   } catch (error) {
     console.log(error);
   }
