1985855cee4ccaca02d6ac15081a560b01819f1a
ruchi <ruchi.kumari63@gmail.com>
2024-05-17 09:58:19 +0530

    with update in backendthat i do not remember

--- controllers/currentTimeController.js
+++ controllers/currentTimeController.js
@@ -1,7 +1,7 @@
 const dayjs = require("dayjs");
 const { StatusCodes } = require("http-status-codes");
 const GetCurrentTime = async (req, res) => {
-  const time = dayjs().add("-6"  ,'h').format();
+  const time = dayjs().add("-8"  ,'h').format();
   // const time = dayjs().format();
 
   res.status(StatusCodes.OK).json({ currentTimeStamp: time });
