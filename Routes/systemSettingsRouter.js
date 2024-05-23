0439162b751295436de08b3748fd9581408b643b
ruchi <ruchi.kumari63@gmail.com>
2024-05-23 17:55:36 +0530

    system settings

--- Routes/systemSettingsRouter.js
+++ Routes/systemSettingsRouter.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const protectedRoute = require("../middleware/protectedResourceAdmin");
+const protectedRouteonlytoken = require("../middleware/protectedResource");
+
+
 const {
   AddSystemSettings,
   GetSystemSettings,
   EditSystemSettings,
 } = require("../controllers/systemSettings");
 
-router.route("/admin/systemsettings").get(GetSystemSettings).post(AddSystemSettings);
+router.route("/admin/systemsettings").get(protectedRouteonlytoken,GetSystemSettings).post(protectedRoute,AddSystemSettings);
 router
   .route("/admin/editsystemsettings/:settings_id")
-  .patch(EditSystemSettings);
+  .patch(protectedRoute,EditSystemSettings);
 
 module.exports = router;
